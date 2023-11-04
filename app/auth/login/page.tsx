655c1029f9de2c9bc606c0a45b04abd15e254cdd
Joel Gurnett <gurnettt@mymacewan.ca>
2023-11-04 17:07:18 -0600

    rough login flow, no style

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import LoginForm from '@/components/loginForm';
+
+const LoginPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Sign in</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+
+export default LoginPage;
